76056ddce1dbbe750e8dcc5a4a5155c3d3c651df
Prathu-Sharma <61193148+Prathu-Sharma@users.noreply.github.com>
2020-06-06 19:21:16 +0530

    Add files via upload

---
+++ my1.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+	$(".d23").fadeIn();
+	$(".d21").fadeIn();
+ 
+ 
+
+    $(".h11").fadeIn(2500);
+    $(".h12").fadeIn(2500);
+	$(".h13").fadeIn(2500);
+ 
+ $(".m1").click(function(){
+ $("nav").slideToggle(2500);
+ });   
+    
+    $(".hd").delay(2000).fadeIn(10000); 
+$(".sd1").slideDown(7000);
+});
